d2cb3febb2d854d30a0328ca93f354ebfadc2d22
alan <a3509305@gmail.com>
2022-07-08 03:21:00 +0800

    [登入] 背景影片動畫請追隨11號白衣人

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -167,11 +167,14 @@
       min-height: 100%;
       video.login-video {
         position: absolute;
+        top: 0;
+        left: 50%;
         min-width: 100%;
         min-height: 100%;
         width: auto;
         height: auto;
         z-index: 1;
+        transform: translateX(-50%);
       }
       &::after {
         content: '';
